f8f9933e025a9e6ed9dec10d190de654d5fa9e7e
Anubhav Jain <ajain@lbl.gov>
2013-08-30 17:10:45 -0700

    Revert "explicitly install paramiko"
    This reverts commit 38bca05f1c77936c8bf771b2d12bbec5f15a1372.

--- MPenv/mpenv.py
+++ MPenv/mpenv.py
@@ -62,7 +62,6 @@ def create_env():
     if not args.dev:
         c.append(('print', 'INSTALLING FireWorks (developer mode)'))
         c.append(("pip install django"))
-        c.append(("pip install paramiko"))
         c.append(("mkdir", "codes"))
         c.append(("cd", 'codes'))
         c.append("git clone git@github.com:materialsproject/fireworks.git")
