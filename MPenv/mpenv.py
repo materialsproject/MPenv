6f8131a8c32915794cfed9865d5b61d24a997e94
Anubhav Jain <ajain@lbl.gov>
2013-12-18 15:53:20 -0800

    add dupefinders to user packages

--- MPenv/mpenv.py
+++ MPenv/mpenv.py
@@ -171,7 +171,7 @@ def create_env():
                 elif envtype == "MP" or envtype == "rubicon":
                     replacements["PACKAGES"] = "\n  - mpworks.firetasks\n  - mpworks.dupefinders\n  - mpworks.examples"
                 if envtype == "rubicon":
-                    replacements["PACKAGES"] += "\n  - rubicon.firetasks"
+                    replacements["PACKAGES"] += "\n  - rubicon.firetasks\n  - rubicon.dupefinders"
 
                 with open(os.path.join(static_dir, 'BASH_template.txt')) as f:
                     t = CustomTemplate(f.read())
